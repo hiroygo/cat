0f344c625e0c7f983a587a655eefe1859eb9f05a
hiroygo <pcxn1a@gmail.com>
2020-09-27 15:39:50 +0900

    update test

--- test.sh
+++ test.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-testdir="test"
 in_files="file1.txt file2.txt"
-expect_file="${testdir}/expect.txt"
-testee_file="${testdir}/testee.txt"
-
-mkdir -p "$testdir"
-
-./main $in_files > "$testee_file"
-cat $in_files > "$expect_file"
-
-diff "$expect_file" "$testee_file"
+diff <(cat $in_files) <(./main $in_files)
